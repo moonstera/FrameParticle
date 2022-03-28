bb27fcbfd10b56ed2c21a71687d9cb9b15b5a008
monstera <monstellar48@gmail.com>
2022-03-28 19:59:38 +0900

    Update FrameParticle.js

--- FrameParticleBP/scripts/FrameParticle.js
+++ FrameParticleBP/scripts/FrameParticle.js
@@ -8,8 +8,8 @@ import {
 } from "mojang-minecraft";
 
 export class FrameParticle {
-  static particle_id = 0;
-  static frame_particles = new Map();
+  static _particle_id = 0;
+  static _frame_particles = new Map();
 
   constructor(dim, loc1, loc2){
     //型チェック
@@ -23,38 +23,38 @@ export class FrameParticle {
       console.error("The specified object is of invalid type. The correct type is \"Location\".");
     }
     
-    this.dim = dim;
-    this.loc1 = new Location(Math.floor(loc1.x), Math.floor(loc1.y), Math.floor(loc1.z));
-    this.loc2 = new Location(Math.floor(loc2.x), Math.floor(loc2.y), Math.floor(loc2.z));
-    this.moving = 0;  //0: false, 1~: true  //座標が変更されたときの残像を軽減するため
-    this.id = FrameParticle.particle_id++;
+    this._dim = dim;
+    this._loc1 = new Location(Math.floor(loc1.x), Math.floor(loc1.y), Math.floor(loc1.z));
+    this._loc2 = new Location(Math.floor(loc2.x), Math.floor(loc2.y), Math.floor(loc2.z));
+    this._moving = 0;  //0: false, 1~: true  //座標が変更されたときの残像を軽減するため
+    this._id = FrameParticle._particle_id++;
   }
 
   show(){
-    if(!FrameParticle.frame_particles.has(this.id)){
-      FrameParticle.frame_particles.set(this.id, this);
+    if(!FrameParticle._frame_particles.has(this._id)){
+      FrameParticle._frame_particles.set(this._id, this);
     }
     return this;
   }
 
   get dimension(){
-    return this.dim;
+    return this._dim;
   }
 
   get location1(){
-    return this.loc1;
+    return this._loc1;
   }
 
   get location2(){
-    return this.loc2;
+    return this._loc2;
   }
 
   set location1(loc){
     if(loc instanceof Location){
       let new_loc = new Location(Math.floor(loc.x), Math.floor(loc.y), Math.floor(loc.z));
-      if(!this.loc1.equals(new_loc)){
-        this.loc1 = new_loc;
-        this.moving = 1;
+      if(!this._loc1.equals(new_loc)){
+        this._loc1 = new_loc;
+        this._moving = 1;
       }
     } else {
       console.error("The specified object is of invalid type. The correct type is \"Location\".");
@@ -64,9 +64,9 @@ export class FrameParticle {
   set location2(loc){
     if(loc instanceof Location){
       let new_loc = new Location(Math.floor(loc.x), Math.floor(loc.y), Math.floor(loc.z));
-      if(!this.loc2.equals(new_loc)){
-        this.loc2 = new_loc;
-        this.moving = 1;
+      if(!this._loc2.equals(new_loc)){
+        this._loc2 = new_loc;
+        this._moving = 1;
       }
     } else {
       console.error("The specified object is of invalid type. The correct type is \"Location\".");
@@ -74,13 +74,13 @@ export class FrameParticle {
   }
 
   delete(){
-    if(FrameParticle.frame_particles.has(this.id))FrameParticle.frame_particles.delete(this.id);
+    if(FrameParticle._frame_particles.has(this._id))FrameParticle._frame_particles.delete(this._id);
   }
 
 }
 
 world.events.tick.subscribe(() => {
-  FrameParticle.frame_particles.forEach((particle, id) => {
+  FrameParticle._frame_particles.forEach((particle, id) => {
     let x = Math.min(particle.location1.x, particle.location2.x);
     let y = Math.min(particle.location1.y, particle.location2.y);
     let z = Math.min(particle.location1.z, particle.location2.z);
@@ -90,11 +90,11 @@ world.events.tick.subscribe(() => {
     let size_z = Math.abs(particle.location1.z - particle.location2.z)+1;
 
     let molang = new MolangVariableMap()
-    .setVector3("variable.id", new Vector(id, particle.moving, 0))
+    .setVector3("variable.id", new Vector(id, particle._moving, 0))
     .setVector3("variable.size", new Vector(size_x, size_y, size_z));
     particle.dimension.spawnParticle("frame_particle:creater", new Location(x, y, z), molang);
-    if(particle.moving){
-      particle.moving = 0;
+    if(particle._moving > 0){
+      particle._moving = 0;
     }
   });
 });
